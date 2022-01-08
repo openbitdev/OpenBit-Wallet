c43657aea56046b181686f64508d73eafd46eff2
MinhLe <minhle2994@gmail.com>
2022-01-08 12:53:58 +0700

    Add crowdloan related APIs

--- packages/dotsama-explorer/README.md
+++ packages/dotsama-explorer/README.md
@@ -80,3 +80,101 @@ unsubscribe()
 ```
 #### Note
 - This library only return balances of native token of each chains (will support other assets in the future)
+
+**Get all crowdloan contribution of an address in Polkadot and/or Kusama**
+```javascript
+import {getAllCrowdloanContribution} from "./src/controllers/apis.js";
+
+// Only Polkadot, Kusama
+const targetChains = [
+    {chainId: 0, paraId: 0},
+    {chainId: 2, paraId: 2},
+]
+
+await getAllCrowdloanContribution(targetChains, '0x00')
+
+// Will output an array of crowdloan contribution like this:
+// [
+//     {
+//         chainId: 0,
+//         paraId: 2000,
+//         token: 'DOT',
+//         tokenDecimals: 10,
+//         contribution: 999999,
+//     },
+//     {
+//         chainId: 0,
+//         paraId: 2002,
+//         token: 'DOT',
+//         tokenDecimals: 10,
+//         contribution: 999999,
+//     },
+//     ....
+//     {
+//         chainId: 2,
+//         paraId: 2000,
+//         token: 'KSM',
+//         tokenDecimals: 12,
+//         contribution: 1111,
+//     },
+//     {
+//         chainId: 2,
+//         paraId: 2002,
+//         token: 'KSM',
+//         tokenDecimals: 12,
+//         contribution: 1111,
+//     }
+//     ....
+// ]
+```
+
+**Get all crowdloan statistics of parachains in Polkadot and/or Kusama**
+```javascript
+import {getAllCrowdloanStats} from "./src/controllers/apis.js";
+
+// Only Polkadot, Kusama
+const targetChains = [
+    {chainId: 0, paraId: 0},
+    {chainId: 2, paraId: 2},
+]
+
+await getAllCrowdloanStats(targetChains)
+
+// Will output an array of crowdloan statistics like this:
+// [
+//     {
+//         chainId: 0,
+//         paraId: 2000,
+//         token: 'DOT',
+//         tokenDecimals: 10,
+//         raised: 32515980.232357625,
+//         cap: 50000000,
+//     },
+//     {
+//         chainId: 0,
+//         paraId: 2002,
+//         token: 'DOT',
+//         tokenDecimals: 10,
+//         raised: 9752487.426803853,
+//         cap: 50000000,
+//     },
+//     ....
+//     {
+//         chainId: 2,
+//         paraId: 2000,
+//         token: 'KSM',
+//         tokenDecimals: 12,
+//         raised: 501137.6619100505,
+//         cap: 1500000,
+//     },
+//     {
+//         chainId: 2,
+//         paraId: 2024,
+//         token: 'KSM',
+//         tokenDecimals: 12,
+//         raised: 47425.526242400854,
+//         cap: 100000,
+//     },
+//     ....
+// ]
+```
--- packages/dotsama-explorer/src/controllers/apis.js
+++ packages/dotsama-explorer/src/controllers/apis.js
@@ -6,6 +6,7 @@ export const getChainMetadata = ({chainId, paraId}) => {
     return loadJSON(manifestPath)
 }
 
+
 export const getBalances = async (targetChains, address) => {
     if (!isValidAddress(address)) {
         console.log('Invalid address.')
@@ -121,4 +122,121 @@ export const subscribeBalances = async (targetChains, address, callback) => {
 
     return () =>
         unsubscribeCallbacks.map(unsubscribeCallback => unsubscribeCallback())
+}
+
+
+export const getAllCrowdloanContribution = async (targetChains, address) => {
+    if (!isValidAddress(address)) {
+        console.log('Invalid address.')
+        return null
+    }
+
+    if (targetChains.length <= 0) {
+        console.log('Must pass at least 1 chainId.')
+        return null
+    }
+
+    let chainDict = {}
+    targetChains.map((item, i) => {
+        const chainMetadata = getChainMetadata({chainId: item.chainId, paraId: item.paraId})
+        chainDict[i] = {chainId: item.chainId, paraId: item.paraId, ...chainMetadata}
+    })
+
+    const apis = await connectChains(targetChains)
+
+
+    // Get all parachain paraIds
+    let chainOrderAllParaIdsMap = []
+    // noinspection JSUnresolvedVariable
+    const records = await Promise.all(apis.map(api => api.query.paras.paraLifecycles.entries()))
+    records.map(allParaIds => {
+        chainOrderAllParaIdsMap.push(allParaIds.map(([key, _]) => key.args[0].toString()))
+    })
+
+    // Get all crowdloan contributions
+    const hexAddress = await apis[0].createType('AccountId', address).toHex()
+    let jobOrderChainOrderMap = []
+    let jobOrderParaIdMap = []
+    let jobs = []
+    apis.map((api, i) => {
+        chainOrderAllParaIdsMap[i].map(paraId => {
+            jobs.push(api.derive.crowdloan.ownContributions(paraId, [hexAddress]))
+            jobOrderChainOrderMap.push(i)
+            jobOrderParaIdMap.push(paraId)
+        })
+    })
+    const crowdloanContributions = await Promise.all(jobs)
+
+    // Parsing crowdloan contribution
+    let results = []
+    crowdloanContributions.map((item, i) => {
+        const currentChain = chainDict[jobOrderChainOrderMap[i]]
+        const paraId = jobOrderParaIdMap[i]
+        // noinspection JSUnresolvedVariable
+        results.push({
+            chainId: currentChain.chainId,
+            paraId: paraId,
+            token: currentChain.nativeToken,
+            tokenDecimals: currentChain.tokenDecimals,
+            contribution: toUnit(item[hexAddress], currentChain.tokenDecimals),
+        })
+    })
+
+    apis.map(api => api.disconnect())
+    return results
+}
+
+
+export const getAllCrowdloanStats = async (targetChains) => {
+    if (targetChains.length <= 0) {
+        console.log('Must pass at least 1 chainId.')
+        return null
+    }
+
+    let chainDict = {}
+    targetChains.map((item, i) => {
+        const chainMetadata = getChainMetadata({chainId: item.chainId, paraId: item.paraId})
+        chainDict[i] = {chainId: item.chainId, paraId: item.paraId, ...chainMetadata}
+    })
+
+    const apis = await connectChains(targetChains)
+
+    // Get all parachain paraIds
+    let chainOrderAllParaIdsMap = []
+    // noinspection JSUnresolvedVariable
+    const records = await Promise.all(apis.map(api => api.query.paras.paraLifecycles.entries()))
+    records.map(allParaIds => {
+        chainOrderAllParaIdsMap.push(allParaIds.map(([key, _]) => key.args[0].toString()))
+    })
+
+    // Get all crowdloan stats
+    let jobs = []
+    apis.map((api, i) => {
+        // noinspection JSUnresolvedVariable
+        jobs.push(api.query.crowdloan.funds.multi(chainOrderAllParaIdsMap[i]))
+    })
+    const crowdloanStats = await Promise.all(jobs)
+
+    // Parsing crowdloan stats
+    let results = []
+    crowdloanStats.map((records, i) => {
+        const currentChain = chainDict[i]
+        records.map((record, j) => {
+            const paraId = chainOrderAllParaIdsMap[i][j]
+            record = record.toHuman()
+            if (record !== null)
+                // noinspection JSUnresolvedVariable
+                results.push({
+                    chainId: currentChain.chainId,
+                    paraId: paraId,
+                    token: currentChain.nativeToken,
+                    tokenDecimals: currentChain.tokenDecimals,
+                    raised: toUnit(record.raised.split(',').join(''), currentChain.tokenDecimals),
+                    cap: toUnit(record.cap.split(',').join(''), currentChain.tokenDecimals),
+                })
+        })
+    })
+
+    apis.map(api => api.disconnect())
+    return results
 }
