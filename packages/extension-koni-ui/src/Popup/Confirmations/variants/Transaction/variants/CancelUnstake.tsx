f819f4645b88c0375bbbbcadb3c7eaedb49827b0
lw <linhtm@cdmteck.com>
2024-01-16 17:28:33 +0700

    [Issue-2361] Update confirmations

--- packages/extension-koni-ui/src/Popup/Confirmations/variants/Transaction/variants/CancelUnstake.tsx
+++ packages/extension-koni-ui/src/Popup/Confirmations/variants/Transaction/variants/CancelUnstake.tsx
@@ -1,6 +1,7 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { RequestStakeCancelWithdrawal } from '@subwallet/extension-base/types';
 import CommonTransactionInfo from '@subwallet/extension-koni-ui/components/Confirmation/CommonTransactionInfo';
 import MetaInfo from '@subwallet/extension-koni-ui/components/MetaInfo/MetaInfo';
 import useGetNativeTokenBasicInfo from '@subwallet/extension-koni-ui/hooks/common/useGetNativeTokenBasicInfo';
@@ -15,12 +16,12 @@ type Props = BaseTransactionConfirmationProps;
 
 const Component: React.FC<Props> = (props: Props) => {
   const { className, transaction } = props;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const data = transaction.data;
+
+  const data = transaction.data as RequestStakeCancelWithdrawal;
 
   const { t } = useTranslation();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const { decimals, symbol } = useGetNativeTokenBasicInfo(data.chain);
+
+  const { decimals, symbol } = useGetNativeTokenBasicInfo(data.selectedUnstaking.chain);
 
   return (
     <div className={CN(className)}>
