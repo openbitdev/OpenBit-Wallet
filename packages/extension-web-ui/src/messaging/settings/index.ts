a5a9ea048063029dd3c9100babef2fa917983e47
nguyentiendung <dungtpa2@gmail.com>
2024-01-31 11:12:32 +0700

    Refactor webapp: messaging

--- packages/extension-web-ui/src/messaging/settings/index.ts
+++ packages/extension-web-ui/src/messaging/settings/index.ts
@@ -1,4 +1,7 @@
 // Copyright 2019-2022 @subwallet/extension-web-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export * from "@subwallet/extension-koni-ui/messaging/settings/index";
+export * from './asset';
+export * from './auth';
+export * from './base';
+export * from './chain';
