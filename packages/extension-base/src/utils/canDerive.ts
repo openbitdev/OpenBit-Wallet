11c0fd0a079c2a860839e870b187ce9ad89945b2
lw <linhtm@cdmteck.com>
2024-03-28 14:30:13 +0700

    Refactor libs

--- packages/extension-base/src/utils/canDerive.ts
+++ packages/extension-base/src/utils/canDerive.ts
@@ -4,5 +4,5 @@
 import type { KeypairType } from '@subwallet/keyring/types';
 
 export function canDerive (type?: KeypairType): boolean {
-  return !!type && ['ed25519', 'sr25519', 'ecdsa', 'ethereum', 'bitcoin-44', 'bitcoin-84', 'bitcoin-86'].includes(type);
+  return !!type && ['ed25519', 'sr25519', 'ecdsa', 'ethereum', 'bitcoin-44', 'bitcoin-84', 'bitcoin-86', 'bittest-44', 'bittest-84', 'bittest-86'].includes(type);
 }
