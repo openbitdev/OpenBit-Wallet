efd3ca50be27b1e10ac0d336a7ee9d4b76b451c1
lw <linhtm@cdmteck.com>
2024-04-27 17:34:49 +0700

    [Issue-54] Update logic for network manager

--- packages/extension-koni-ui/src/Popup/Settings/Chains/ManageChains.tsx
+++ packages/extension-koni-ui/src/Popup/Settings/Chains/ManageChains.tsx
@@ -8,9 +8,9 @@ import { DataContext } from '@subwallet/extension-koni-ui/contexts/DataContext';
 import { ChainInfoWithState, useFilterModal, useTranslation } from '@subwallet/extension-koni-ui/hooks';
 import useChainInfoWithStateAndStatus, { ChainInfoWithStateAndStatus } from '@subwallet/extension-koni-ui/hooks/chain/useChainInfoWithStateAndStatus';
 import { ManageChainsParam, ThemeProps } from '@subwallet/extension-koni-ui/types';
-import { Icon, ModalContext, SwList } from '@subwallet/react-ui';
+import { ButtonProps, Icon, ModalContext, SwList } from '@subwallet/react-ui';
 import { SwListSectionRef } from '@subwallet/react-ui/es/sw-list';
-import { FadersHorizontal } from 'phosphor-react';
+import { FadersHorizontal, Plus } from 'phosphor-react';
 import React, { SyntheticEvent, useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
@@ -101,6 +101,23 @@ function Component ({ className = '' }: Props): React.ReactElement<Props> {
     );
   }, []);
 
+  const subHeaderButton: ButtonProps[] = useMemo(() => {
+    return [
+      {
+        icon: (
+          <Icon
+            phosphorIcon={Plus}
+            size='md'
+            type='phosphor'
+          />
+        ),
+        onClick: () => {
+          navigate('/settings/chains/import', { state: { isExternalRequest: false } });
+        }
+      }
+    ];
+  }, [navigate]);
+
   const onBack = useCallback(() => {
     navigate(-1);
   }, [navigate]);
@@ -130,6 +147,7 @@ function Component ({ className = '' }: Props): React.ReactElement<Props> {
         showSubHeader={true}
         subHeaderBackground={'transparent'}
         subHeaderCenter={true}
+        subHeaderIcons={subHeaderButton}
         subHeaderPaddingVertical={true}
         title={t<string>('Manage networks')}
       >
