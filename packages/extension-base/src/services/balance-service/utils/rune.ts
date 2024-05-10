0c4438b6e73b854fa5916b8829aedfe0c72ee4db
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-05-10 12:17:57 +0700

    [Issue-70] add util function to handle rune spacer & get all collection rune

---
+++ packages/extension-base/src/services/balance-service/utils/rune.ts
@@ -0,0 +1,73 @@
+// Copyright 2019-2022 @subwallet/extension-base authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// export function insertRuneSlugSpacer (runeSlug: string, spacerList: number[], char: string) {
+//   function insertCharAtIndex (str: string, index: number, char: string): string {
+//     return str.slice(0, index) + char + str.slice(index);
+//   }
+//
+//   if (!spacerList.length) {
+//     return runeSlug;
+//   }
+//
+//   const slugSplit = runeSlug.split('-');
+//   let runeName = slugSplit[2];
+//
+//   for (let i = spacerList.length - 1; i >= 0; i--) {
+//     const index = spacerList[i];
+//
+//     runeName = insertCharAtIndex(runeName, index, char);
+//   }
+//
+//   slugSplit[2] = runeName;
+//
+//   return slugSplit.join('-');
+// }
+
+export function insertRuneSpacer (runeName: string, spacerList: number[], char = '*') {
+  function insertCharAtIndex (str: string, index: number, char: string): string {
+    return str.slice(0, index) + char + str.slice(index);
+  }
+
+  if (!spacerList.length) {
+    return runeName;
+  }
+
+  for (let i = spacerList.length - 1; i >= 0; i--) {
+    const index = spacerList[i];
+
+    runeName = insertCharAtIndex(runeName, index + 1, char);
+  }
+
+  return runeName;
+}
+
+export function decodeRuneSpacer (spacerInt: number) {
+  function decimalToBinaryString (decimal: number) {
+    const binaryString: string = (decimal >>> 0).toString(2);
+
+    return binaryString;
+  }
+
+  function binaryStringToSpacerList (binaryString: string) {
+    const reverseStringList = binaryString.split('').reverse().join('');
+
+    return findAllIndices(reverseStringList, '1');
+  }
+
+  function findAllIndices (input: string, target: string): number[] {
+    const regex = new RegExp(target, 'g');
+    const indices: number[] = [];
+    let match: RegExpExecArray | null;
+
+    while ((match = regex.exec(input)) !== null) {
+      indices.push(match.index);
+    }
+
+    return indices;
+  }
+
+  const binaryString = decimalToBinaryString(spacerInt);
+
+  return binaryStringToSpacerList(binaryString);
+}
