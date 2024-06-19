42a6cec3162ef627995b3251ea4e5a7b6f8b26f3
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-06-19 16:51:01 +0700

    [Issue-189] optimize speed get bitcoin balance

--- packages/extension-base/src/services/chain-service/handler/bitcoin/strategy/BlockStream/types.ts
+++ packages/extension-base/src/services/chain-service/handler/bitcoin/strategy/BlockStream/types.ts
@@ -32,7 +32,11 @@ export interface BitcoinAddressSummaryInfo {
     spent_txo_count: number,
     spent_txo_sum: number,
     tx_count: number
-  }
+  },
+  balance: number,
+  balance_inscription: number,
+  balance_rune: number,
+  total_inscription: number
 }
 
 // todo: combine RunesByAddressResponse & RunesCollectionInfoResponse
